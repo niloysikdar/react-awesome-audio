e4f7cbc363ac86da1054f9e69f215c04b850693c
Niloy Sikdar <niloysikdar30@gmail.com>
2022-01-01 20:45:17 +0530

    Added more comments and other keywords

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+/**
+ *
+ * index.ts file
+ * ----
+ * Optimized and Supercharged React hook to play audio without any DOM element 💪🎧
+ * Created with love by Niloy Sikdar
+ *
+ */
+
+// Exporting the useAudio hook
 export { useAudio } from './useAudio';
